092ac4a2b114fece2528e6506fc98b3b6346feaf
maritocuate <mario300@gmail.com>
2023-10-22 16:03:00 -0300

    chore: Memo

--- src/components/Memo.jsx
+++ src/components/Memo.jsx
@@ -1,7 +1,52 @@
+import React, { memo, useState } from 'react'
+
 function Memo () {
+  const [name, setName] = useState('')
+
+  const names = [
+    'Mark',
+    'Eric'
+  ]
+
+  const getName = () => {
+    const random = Math.floor(Math.random() * names.length)
+    return names[random]
+  }
+
+  const clearName = () => {
+    setName('')
+  }
+
+  const obtainName = () => {
+    setName(getName())
+  }
+
   return (
-    <div>Memo</div>
+    <>
+      <h2>Memo</h2>
+      <RandomNames name={name} clearName={clearName} />
+      <button onClick={obtainName}>
+        Obtain Name
+      </button>
+    </>
   )
 }
 
 export default Memo
+
+export const NameComponent = props => {
+  return (
+    <>
+      <h2>{props.name}</h2>
+      <button onClick={props.clearName}>
+        Clear
+      </button>
+    </>
+  )
+}
+
+function NamesAreEqual (prevProps, nextProps) {
+  return prevProps.name === nextProps.name
+}
+
+export const RandomNames = memo(NameComponent, NamesAreEqual)
